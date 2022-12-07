06d9b2dc6d40389a988731bd9929f9fe28b7a52c
isbecky27 <is.becky27@gmail.com>
2022-12-08 01:29:49 +0800

    the version for sequences

--- main.py
+++ main.py
@@ -30,45 +30,58 @@ def get_parameters(tracker, tracker_params):
     return params
 
 ## Get Parameters
-init, update = False, False
 cfg = get_params('./config.yaml')
 tracker_cfg = get_parameters(cfg.tracker, cfg.tracker_params)
 
 ## Initialize
-tracker = OSTrack(tracker_cfg, None, threshold=1.0)
+tracker = OSTrack(tracker_cfg, None, threshold=2.0)
 
-for i in range(1):
+seq = 'person20'
+prev_bbox = None
+init, update = False, False
+for idx in range(1, len(os.listdir(f'./data/{seq}/'))+1):
 
     ## Detection
     if not init:
+        cfg.source = f'./data/{seq}/{str(idx).zfill(6)}.jpg'
         bboxes = detect(cfg)
         if bboxes == []:
             continue
     elif update:
+        cfg.source = f'./data/{seq}/{str(idx).zfill(6)}.jpg'
         bboxes = detect(cfg)
+        print("Detection:", idx)
 
+    ## Manual Selection
+    if not init:
+        bbox = {'init_bbox': [453,367,69,159]}
+        prev_bbox = bbox['init_bbox']
     ## Selection Module
-    # bbox = {'init_bbox': [691, 365, 36, 22]}
-    bboxes = np.array(bboxes)
-    bboxes[:, 2] -= bboxes[:, 0]
-    bboxes[:, 3] -= bboxes[:, 1]
-    # bbox = {'init_bbox': bboxes[0]}
-    bbox = {'init_bbox': selection_bbox(bboxes, [691, 365, 36, 22])}
+    else:
+        bboxes = np.array(bboxes)
+        bboxes[:, 2] -= bboxes[:, 0]
+        bboxes[:, 3] -= bboxes[:, 1]
+        target = selection_bbox(bboxes, prev_bbox)
+        bbox = {'init_bbox': target}
 
     ## Tracker
-    img = cv2.imread('./test/000001.jpg')
-    img2 = cv2.imread('./test/000002.jpg')
-    
-    if (not init) or update:
+    img = cv2.imread(f'./data/{seq}/{str(idx).zfill(6)}.jpg')
+    if (not init) or (update and (target is not None)):
         init, update = True, False
         tracker.initialize(img, bbox)
-    output = tracker.track(img2)
+    output = tracker.track(img)
     update = output['update_template']
-    print(output)
+    if update:
+        print("update:", idx)
+    else:
+        prev_bbox = output['target_bbox']
 
     ## Show Results
+    result_path = f'./output/{seq}'
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
     box = output['target_bbox']
     box = [int(float(i)) for i in box]
     color = (255, 0, 0)
-    image = cv2.rectangle(img2, (box[0], box[1]), (box[0]+box[2], box[1]+box[3]), color, thickness = 2)
-    cv2.imwrite('test.png', image)
+    image = cv2.rectangle(img, (box[0], box[1]), (box[0]+box[2], box[1]+box[3]), color, thickness = 2)
+    cv2.imwrite(f'{result_path}/{str(idx).zfill(6)}.jpg', image)
